893c59a244edc421dd72d979e54b1654973c0ad8
TomDuaner <92232038+tomduaner@users.noreply.github.com>
2021-10-10 15:46:38 -0400

    Change to Ideabase
    Changing default react template into Ideabase

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hello World!
         </p>
+        <p>
+          ;)
+          </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ideabase.xyz"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ideabase
+  
         </a>
       </header>
     </div>
